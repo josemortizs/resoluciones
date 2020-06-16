cf82996589c18f94306441fc53f99df388e49679
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2020-06-16 21:52:26 +0200

    Pruebas sobre la funcionalidad creada

--- init.py
+++ init.py
@@ -1,5 +1,12 @@
 from Models.gestorresoluciones import GestorResoluciones
+from Models.resolucion import Resolucion
 
 
 gestor = GestorResoluciones()
+gestor.imprimeListadoResoluciones()
+
+resolucion = Resolucion(1280,1024,"PC","SXGA")
+gestor.agregaResolucion(resolucion)
+
+print("Resoluciones ahora: ")
 gestor.imprimeListadoResoluciones()
