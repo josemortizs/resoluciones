1943387c433fc07a9445a672195cbe27e8f30ca6
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2020-06-21 20:52:55 +0200

    inicial

--- Scripts/gestorresoluciones.py
+++ Scripts/gestorresoluciones.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  ortizsanchezdev
+
+# Author: José Manuel Ortiz Sánchez <josemortizs@gmail.com>
+# Maintainer: José Manuel Ortiz Sánchez <josemortizs@gmail.com>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with urls; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+
 # Importamos la clase Resolucion()
 from resolucion import Resolucion
 # Importamos la librería "csv" para trabajar con este tipo de archivos
--- Scripts/resolucion.py
+++ Scripts/resolucion.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  ortizsanchezdev
+
+# Author: José Manuel Ortiz Sánchez <josemortizs@gmail.com>
+# Maintainer: José Manuel Ortiz Sánchez <josemortizs@gmail.com>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with urls; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 class Resolucion():
     
     """ **************************************************************** 
